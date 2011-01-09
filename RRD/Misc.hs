8c223222cc8a770e1b9da5cdb0e007b31fdeaf4b
zs <moreilcon@gmail.com>
2011-01-09 14:31:23 +0300

    +rrd_first

--- RRD/Misc.hs
+++ RRD/Misc.hs
@@ -1,11 +1,15 @@
-module RRD.Misc where
+module RRD.Misc
+    ( lastUpdate
+    , first
+    ) where
 import Bindings.Librrd
 import Foreign.C.String (withCString)
 import Control.Exception (bracket)
 import Foreign.Marshal.Utils (with)
-import Foreign.C.Error (throwErrnoIf_)
+import Foreign.C.Error (throwErrnoIf_, throwErrnoIf)
 import Foreign.Marshal (malloc, free)
 import Foreign.Storable (peek)
+import Data.Maybe (fromMaybe)
 
 
 -- TODO make sure it's leak free, it's probably so, but it's better to make some actuall tests
@@ -20,6 +24,7 @@ lastUpdate path =
                             (c'rrd_lastupdate_r c'path lastUpdate_ret dsCount_ret dsNames_ret lastDs_ret)
                         fromEnum `fmap` peek lastUpdate_ret
 
-
-
-
+first :: FilePath -> Maybe Int -> IO Int
+first path rraIndex = withCString path $ \c'path ->
+    throwErrnoIf (== -1) ("rrd_first " ++ show path) $
+        fromEnum `fmap` c'rrd_first_r c'path (toEnum $ fromMaybe 0 rraIndex)
